5261c1311f248e255860c990c9fd1b781df66886
ceciliaccwei <ceciliawei@yahoo.com>
2020-01-06 17:44:08 -0700

    script done

--- script.py
+++ script.py
@@ -2,3 +2,5 @@ import requests
 print(requests.__version__)
 response = requests.get('http://google.com/')
 print(response.status_code)
+response = requests.get('https://raw.githubusercontent.com/ceciliaccwei/CMPUT404Lab1/master/script.py')
+print(response.content)
